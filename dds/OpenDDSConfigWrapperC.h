5385e7ef973aebfceb8362a8b84ecc978e1fc6cb
Justin R. Wilson <wilsonj@unityfoundation.io>
2024-05-22 16:02:46 -0500

    Extend to IDL

---
+++ dds/OpenDDSConfigWrapperC.h
@@ -0,0 +1,11 @@
+/*
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#ifndef OPENDDS_DDS_OPENDDSCONFIG_WRAPPER_C_H
+#define OPENDDS_DDS_OPENDDSCONFIG_WRAPPER_C_H
+
+#include "OpenDDSConfigWrapper.h"
+
+#endif
