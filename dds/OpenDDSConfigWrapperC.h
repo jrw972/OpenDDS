9057d5510e09ecabe22182189dd28be0a5818249
Justin R. Wilson <wilsonj@unityfoundation.io>
2024-05-23 11:50:56 -0500

    Satisfy lint

--- dds/OpenDDSConfigWrapperC.h
+++ dds/OpenDDSConfigWrapperC.h
@@ -3,8 +3,8 @@
  * See: http://www.opendds.org/license.html
  */
 
-#ifndef OPENDDS_DDS_OPENDDSCONFIG_WRAPPER_C_H
-#define OPENDDS_DDS_OPENDDSCONFIG_WRAPPER_C_H
+#ifndef OPENDDS_CONFIG_WRAPPER_C_H
+#define OPENDDS_CONFIG_WRAPPER_C_H
 
 #include "OpenDDSConfigWrapper.h"
 
